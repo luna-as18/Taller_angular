b65e8671feb8c68ba547ec66aee0e683640b6397
luna-as18 <85699323+luna-as18@users.noreply.github.com>
2022-10-31 21:11:16 -0500

    avances

---
+++ src/app/serie/dataSeries.ts
@@ -0,0 +1,23 @@
+import { Serie } from './serie';
+
+export const defaultseries = [
+  new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
+  "https://www.amc.com/shows/breaking-bad","/Assets/Breaking-bad.jpg") ,
+
+  new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary",
+  "https://www.netflix.com/co/title/70242311","/Assets/Orange-is-the-new-black.jpg"),
+
+  new Serie (3, "Game of Thrones","HBO", 7, "In the mythical continent of Westeros, nine families of higher nobility (Targaryen, Lannisters, Starks, Tyrell, Martell, Greyjoys, Baratheons and Boltons) scramble bitterly to gain power over the seven kingdoms and the Iron throne.",
+      "https://www.hbo.com/game-of-thrones",
+      "/Assets/Game-of-thrones.jpg"),
+
+  new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life.",
+      "https://www.cbs.com/shows/big_bang_theory/about/", "/Assets/The-big-bang-theory.jpg"),
+
+  new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman).",
+      "https://www.bbc.co.uk/programmes/b018ttws", "/Assets/Sherlock.jpg"),
+
+  new Serie (6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
+      "https://www.bbc.co.uk/programmes/p065smy4", "/Assets/A-very-english-scandal.jpg"),
+];
+
